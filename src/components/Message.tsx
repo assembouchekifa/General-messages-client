830fc3b05409eadff8ca1a10688cd8e9c1ad9876
assembouchekifa <147662333+assembouchekifa@users.noreply.github.com>
2024-01-23 17:42:07 +0100

    no botton

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -18,8 +18,6 @@ async function getmessage() {
 function Message() {
   const [messages, setMessages] =
     useState<{ _id: string; message: string; __v: number }[]>();
-  const [def, serDef] = useState(false);
-
   async function clc() {
     const data: { _id: string; message: string; __v: number }[] =
       await getmessage();
@@ -29,26 +27,11 @@ function Message() {
     scrollTo({ top: document.documentElement.scrollHeight });
   }, [messages]);
 
-  useEffect(() => {
-    clc();
-    setInterval(() => {
-      if (def === false) {
-        serDef(true);
-      } else {
-        serDef(false);
-      }
-    }, 10000);
-  });
-
-  useEffect(() => {
-    clc();
-  }, [def]);
-
   return (
     <>
-      {/* <button className="fixed left-0 top-12 p-2 size-8 " onClick={clc}>
+      <button className="fixed left-0 top-12 p-2 size-8 " onClick={clc}>
         <IoReloadCircle className="absolute h-full w-full" />
-      </button> */}
+      </button>
       {messages?.map((e) => {
         return (
           <div
